57929007da6ea009370d5f2545782ce70eddaad7
Noah Visness <nvisness@gmail.com>
2016-01-13 19:00:43 -0600

    Add confirmation that data is correct

--- src/GitDemo.java
+++ src/GitDemo.java
@@ -6,32 +6,34 @@ public class GitDemo {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		System.out.print("What's your name? ");
-		String name = in.nextLine();
+		while (true) {
 		
-		System.out.println("Hello, " + name);
+			System.out.print("What's your name? ");
+			String name = in.nextLine();
+			
+			System.out.print("What grade are you in? ");
+			int grade = in.nextInt();
+			in.nextLine();
+			
+			System.out.print("What is your favorite water buffalo? ");
+			String waterbuffalo = in.nextLine();
+			
+			System.out.print("What is your favorite type of pizza? ");
+			String pizza = in.nextLine();
+	
+			System.out.print("What is your favorite movie? ");
+			String movie = in.nextLine();
+			
+			System.out.println("Name: " + name + " Grade: " + grade + " Favorite water buffalo: " + waterbuffalo + " Favorite pizza: " + pizza + " Favorite movie: " + movie);
+			System.out.print("Is this correct? (y/n) ");
+			String confirm = in.nextLine();
+			if (confirm.equals("y")) {
+				break;
+			}
+			
+		}
 		
-		System.out.print("What grade are you in? ");
-		int grade = in.nextInt();
-		in.nextLine();
-		
-		System.out.println("You are in grade " + grade);
-		
-		System.out.print("What is your favorite water buffalo? ");
-		String waterbuffalo = in.nextLine();
-		
-		System.out.println("Your favorite buffalo is " + waterbuffalo);
-		
-		System.out.print("What is your favorite type of pizza? ");
-		String pizza = in.nextLine();
-		
-		System.out.println("Your favorite type of pizza is " + pizza);
-		
-		System.out.print("What is your favorite movie?");
-		String movie = in.nextLine();
-		
-		System.out.println("Are you sure your favorite movie is " + movie +"?");
-
+		System.out.println("Thank you for your time!");
 		
 		in.close();
 	}
