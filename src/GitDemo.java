1ec212293a6d8608b8e91fe9bcd61a3063612731
Noah Visness <nvisness@gmail.com>
2016-01-16 15:34:02 -0600

    Add three new questions.

--- src/GitDemo.java
+++ src/GitDemo.java
@@ -35,6 +35,39 @@ public class GitDemo {
 		
 		System.out.println("Thank you for your time!");
 		
+		while (true) {
+			System.out.print("Are you part of the Illuminati? (y/n) ");
+			String scrt = in.nextLine();
+			
+			System.out.print("Are you planning on killing anyone today? (y/n) ");
+			String kill = in.nextLine();
+			
+			System.out.print("On a scale of 1 - 1000 how much newb are you? ");
+			int newblvl = in.nextInt();
+			in.nextLine();
+			while (true) {
+				if (newblvl > 1000 || newblvl < 1) {
+					System.out.println("Ha, ha very funny. Now what is it?");
+					newblvl = in.nextInt();
+					in.nextLine();
+				}
+				else {
+					break;
+				}
+			}
+			
+			System.out.println("You claim that your allegiance to the Illuminati is " + scrt + ". When we asked you if you will kill anyone you answered: " + kill + ". Your newb level (that you claim) is:" + newblvl + ". We know that you are a dirty little liar.");
+			System.out.print("Done being interrogated? (y/n) ");
+			String confirm = in.nextLine();
+			if (confirm.equals("y")) {
+				System.out.println("Nope. That means you're hiding something. Interrogations continue.");
+			}
+			else {
+				System.out.println("Thank you for your time. But we have run out of use for you. Kill him.");
+				System.out.println("You have been executed.");
+				break;
+			}
+		}
 		in.close();
 	}
 
