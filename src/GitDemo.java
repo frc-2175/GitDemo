91a7b5c2e1c975d4f500b3806de0507a9e376c9c
Kevin La Casse <wolfklacasse@gmail.com>
2016-02-07 16:56:18 -0600

    Simplify code

--- src/GitDemo.java
+++ src/GitDemo.java
@@ -5,7 +5,7 @@ public class GitDemo {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        String[] stuff = { "Ha, ha very funny. Now what is it?",
+        String[] newbStatements = { "Ha, ha very funny. Now what is it?",
                 "Jokes over, just tell us.", "Do you want me to hurt you?",
                 "You're just a little punk aren't ya. How newby are you?",
                 "..." };
@@ -34,50 +34,50 @@ public class GitDemo {
 
         }
 
-        System.out.println("Thank you for your time!");
-        System.out.println("...Just kidding. We're just getting started.");
+        System.out.println(
+                "Thank you for your time! \n...Just kidding. We're just getting started.");
 
         while (true) {
-            System.out.print("Are you part of the Illuminati? (y/n) ");
-            String illuminati = in.nextLine();
-            if (illuminati.equals("y")) {
-                illuminati = "are";
-            } else {
-                illuminati = "are not";
-            }
 
-            System.out
-                    .print("Are you planning on killing anyone today? (y/n) ");
-            String kill = in.nextLine();
-            if (kill.equals("y")) {
-                kill = "are";
-            } else {
-                kill = "are not";
-            }
+            String[] questions = { "Are you a part of the Illuminati? (y/n) ",
+                    "Are you planning on killing anyone today? (y/n) ",
+                    "On a scale of 1 - 1000 how much newb are you? " };
+            String[] answers = new String[3];
+            for (int i = 0; i < 3; i++) {
+                System.out.println(questions[i]);
+                answers[i] = in.nextLine();
+                if (i == 0 && answers[0].equals("y")) {
+                    answers[i] = "Involved";
+                }
+                if (i == 1 && answers[1].equals("y")) {
+                    answers[i] = "Yes";
+                }
+                if (i == 2) {
+                    int j = 0;
+                    while (true) {
+                        if (Integer.parseInt(answers[i]) >= 1
+                                && Integer.parseInt(answers[i]) <= 1000) {
+                            break;
+                        } else {
+                            System.out.println(newbStatements[j]);
+                            // System.out.println(Integer.parseInt(answers[i]));
+                            answers[i] = in.nextLine();
 
-            System.out.print("On a scale of 1 - 1000 how much newb are you? ");
-            int newblvl = in.nextInt();
-            in.nextLine();
-            int i = 0;
-            while (true) {
-                if (newblvl > 1000 || newblvl < 1) {
-                    System.out.println(stuff[i]);
-                    newblvl = in.nextInt();
-                    in.nextLine();
+                        }
+                        if (j < 4) {
+                            j++;
+                        } else {
+                            j -= 4;
+                        }
+                    }
                 }
 
-                else {
-                    break;
-                }
-                if (i < 4) {
-                    i++;
-                }
             }
 
-            System.out.println("You claim that you " + illuminati
-                    + " a part the Illuminati. You say that you " + kill
-                    + " planning to kill someone today. Your newb level (that you claim) is: "
-                    + newblvl + ". We know that you are a dirty little liar.");
+            System.out.println("Alignment with Illuminati: " + answers[0]
+                    + ". Involved in any gangsta business: " + answers[1]
+                    + ". Your newb level (that you claim) is: " + answers[2]
+                    + ". We know that you are a dirty little liar.");
             System.out.print("Done being interrogated? (y/n) ");
             String confirm = in.nextLine();
             if (confirm.equals("y")) {
